e98105c367289d22d72d7c1de3c00f6cc722853f
makala0 <ejmacala@gmail.com>
2026-03-25 05:27:33 +0100

    init commit

---
+++ src/main/java/cz/amv/hartmann/HartmannApplication.java
@@ -0,0 +1,13 @@
+package cz.amv.hartmann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HartmannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HartmannApplication.class, args);
+	}
+
+}
