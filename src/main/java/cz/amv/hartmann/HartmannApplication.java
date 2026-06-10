ae17ebab4cdf1c95ef4e97c15c1bcd465b86f9c3
makala0 <ejmacala@gmail.com>
2026-06-10 14:04:55 +0200

    FE build + appka

--- src/main/java/cz/amv/hartmann/HartmannApplication.java
+++ src/main/java/cz/amv/hartmann/HartmannApplication.java
@@ -3,10 +3,23 @@ package cz.amv.hartmann;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class HartmannApplication {
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Create a simple GUI frame
+            JFrame frame = new JFrame("AMViewer");
+            JLabel label = new JLabel("Application is running!", SwingConstants.CENTER);
+            frame.add(label);
+
+            // Configure the frame
+            frame.setSize(300, 150);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
         SpringApplication.run(HartmannApplication.class, args);
     }
 }
