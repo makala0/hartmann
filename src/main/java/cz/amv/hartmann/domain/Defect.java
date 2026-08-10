089b5dc4dfbe2b11679ef836b57a36f2f03ebce4
makala0 <ejmacala@gmail.com>
2026-08-10 09:49:24 +0200

    vykreslovani defektu

---
+++ src/main/java/cz/amv/hartmann/domain/Defect.java
@@ -0,0 +1,44 @@
+package cz.amv.hartmann.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Defect {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "item_id", nullable = false, length = 50)
+    private String itemId;
+
+    @NotNull
+    @Column(name = "position_x", nullable = false)
+    private Float positionX;
+
+    @NotNull
+    @Column(name = "position_y", nullable = false)
+    private Float positionY;
+
+    @NotNull
+    @Column(name = "width", nullable = false)
+    private Float width;
+
+    @NotNull
+    @Column(name = "height", nullable = false)
+    private Float height;
+
+    @Size(max = 10)
+    @NotNull
+    @Column(name = "station", nullable = false, length = 10)
+    private String station;
+
+    @Size(max = 10)
+    @NotNull
+    @Column(name = "type", nullable = false, length = 10)
+    private String type;
+}
